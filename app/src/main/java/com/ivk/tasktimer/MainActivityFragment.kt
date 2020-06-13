ee6def82c43622bf38b2e8cfb9930cf8fb314c33
Irina Kolobova <kolobovairinav@gmail.com>
2020-06-12 18:37:24 -0700

    Add Fragment for Main Activity

---
+++ app/src/main/java/com/ivk/tasktimer/MainActivityFragment.kt
@@ -0,0 +1,15 @@
+package com.ivk.tasktimer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+}
