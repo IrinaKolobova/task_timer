8fa0c1ea7fc96d0a582b24a5618143158e6e0f58
Irina Kolobova <kolobovairinav@gmail.com>
2020-07-10 16:55:31 -0700

    Subscribe to the ViewModel

--- app/src/main/java/com/ivk/tasktimer/MainActivityFragment.kt
+++ app/src/main/java/com/ivk/tasktimer/MainActivityFragment.kt
@@ -1,19 +1,32 @@
 package com.ivk.tasktimer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_main.*
-
+/**
+ * A placeholder fragment containing a simple view.
+ */
 private const val TAG = "MainActivityFragment"
 
+@SuppressLint("UseRequireInsteadOfGet")
 class MainActivityFragment : Fragment() {
 
+    private val viewModel by lazy { ViewModelProviders.of(activity!!).get(TaskTimerViewModel::class.java) }
     private val mAdapter = CursorRecyclerViewAdapter(null)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: called")
+        super.onCreate(savedInstanceState)
+        viewModel.cursor.observe(this, Observer { cursor -> mAdapter.swapCursor(cursor)?.close() })
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
