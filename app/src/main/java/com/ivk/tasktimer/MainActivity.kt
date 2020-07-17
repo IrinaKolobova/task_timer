2602bba2964e0685bf3f579b72e5ef1f5ce88945
Irina Kolobova <kolobovairinav@gmail.com>
2020-07-17 00:15:58 -0700

    Add version compatibility for "about" dialog

--- app/src/main/java/com/ivk/tasktimer/MainActivity.kt
+++ app/src/main/java/com/ivk/tasktimer/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.ivk.tasktimer
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +11,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.content_main.*
@@ -124,7 +128,19 @@ class MainActivity : AppCompatActivity(),
 
         val aboutVersion = messageView.findViewById(R.id.about_version) as TextView
         aboutVersion.text = BuildConfig.VERSION_NAME
-//        about_version.text = BuildConfig.VERSION_NAME  <- synthetic import! code will compile but it will crush
+
+        // Use a nullable type: the TextView won't exist on API 21 and higher
+        val aboutUrl: TextView? = messageView.findViewById(R.id.about_url)
+        aboutUrl?.setOnClickListener { v ->
+            val intent = Intent(Intent.ACTION_VIEW)
+            val s = (v as TextView).text.toString()
+            intent.data = Uri.parse(s)
+            try {
+                startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                Toast.makeText(this@MainActivity, R.string.about_url_error, Toast.LENGTH_LONG).show()
+            }
+        }
 
         aboutDialog?.show()
     }
