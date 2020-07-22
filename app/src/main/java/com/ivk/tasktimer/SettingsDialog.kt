cd18ae8da4d39b9e6abf1ec58d3d77fda7c180a3
Irina Kolobova <kolobovairinav@gmail.com>
2020-07-22 01:30:51 -0700

    Update database

--- app/src/main/java/com/ivk/tasktimer/SettingsDialog.kt
+++ app/src/main/java/com/ivk/tasktimer/SettingsDialog.kt
@@ -20,6 +20,7 @@ const val SETTINGS_DEFAULT_IGNORE_LESS_THAN = 0
 //                              0  1  2   3   4   5   6   7   8   9   10  11  12  13   14   15   16   17   18   19   20   21   22   23    24
 private val deltas = intArrayOf(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 900, 1800, 2700)
 
+@Suppress("DEPRECATION")
 class SettingsDialog: AppCompatDialogFragment() {
 
     private val defaultFirstDayOfWeek = GregorianCalendar(Locale.getDefault()).firstDayOfWeek
