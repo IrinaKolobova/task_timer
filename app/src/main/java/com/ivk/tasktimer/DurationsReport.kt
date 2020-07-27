70be69798a8f610b613a1a3fa4ca3ea6cbc44d33
Irina Kolobova <kolobovairinav@gmail.com>
2020-07-27 16:31:30 -0700

    Add sorting within the app for the report data

--- app/src/main/java/com/ivk/tasktimer/DurationsReport.kt
+++ app/src/main/java/com/ivk/tasktimer/DurationsReport.kt
@@ -1,20 +1,17 @@
 package com.ivk.tasktimer
 
-import android.database.Cursor
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.task_durations.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 private const val TAG = "DurationsReport"
 
-class DurationsReport : AppCompatActivity() {
+class DurationsReport : AppCompatActivity(),
+    View.OnClickListener {
 
     private val viewModel by lazy { ViewModelProviders.of(this).get(DurationsViewModel::class.java) }
 
@@ -31,10 +28,24 @@ class DurationsReport : AppCompatActivity() {
         td_list.adapter = reportAdapter
 
         viewModel.cursor.observe(this, Observer { cursor -> reportAdapter.swapCursor(cursor)?.close() })
+
+        // Set the listener for the buttons so we can sort the report.
+        td_name_heading.setOnClickListener(this)
+        td_description_heading?.setOnClickListener(this)    // description will not be present in portrait
+        td_start_heading.setOnClickListener(this)
+        td_duration_heading.setOnClickListener(this)
     }
 
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.td_name_heading -> viewModel.sortOrder = SortColumns.NAME
+            R.id.td_description_heading -> viewModel.sortOrder = SortColumns.DESCRIPTION
+            R.id.td_start_heading -> viewModel.sortOrder = SortColumns.START_DATE
+            R.id.td_duration_heading -> viewModel.sortOrder = SortColumns.DURATION
+        }
+    }
 
-//    override fun onDestroy() {
+    //    override fun onDestroy() {
 //        reportAdapter.swapCursor(null)?.close()
 //        super.onDestroy()
 //    }
