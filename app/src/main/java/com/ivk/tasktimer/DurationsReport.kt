75d285cace849c01a1002b772b51ce604f3a9ed7
Irina Kolobova <kolobovairinav@gmail.com>
2020-07-27 16:18:52 -0700

    Create Durations ViewModel

--- app/src/main/java/com/ivk/tasktimer/DurationsReport.kt
+++ app/src/main/java/com/ivk/tasktimer/DurationsReport.kt
@@ -3,9 +3,10 @@ package com.ivk.tasktimer
 import android.database.Cursor
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.task_durations.*
 import kotlinx.coroutines.GlobalScope
@@ -13,24 +14,11 @@ import kotlinx.coroutines.launch
 
 private const val TAG = "DurationsReport"
 
-enum class SortColumns {
-    NAME,
-    DESCRIPTION,
-    START_DATE,
-    DURATION
-}
-
 class DurationsReport : AppCompatActivity() {
 
+    private val viewModel by lazy { ViewModelProviders.of(this).get(DurationsViewModel::class.java) }
+
     private val reportAdapter by lazy { DurationsRVAdapter(this, null) }
-
-    var databaseCursor: Cursor? = null
-
-    val sortOrder = SortColumns.NAME
-
-    private val selection = "${DurationsContract.Columns.START_TIME} Between ? AND ?"
-    private var selectionArgs = arrayOf("1556668800", "1559347199")
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,29 +30,9 @@ class DurationsReport : AppCompatActivity() {
         td_list.layoutManager = LinearLayoutManager(this)
         td_list.adapter = reportAdapter
 
-        loadData()
+        viewModel.cursor.observe(this, Observer { cursor -> reportAdapter.swapCursor(cursor)?.close() })
     }
 
-    private fun loadData() {
-        val order = when (sortOrder) {
-            SortColumns.NAME -> DurationsContract.Columns.NAME
-            SortColumns.DESCRIPTION -> DurationsContract.Columns.DESCRIPTION
-            SortColumns.START_DATE -> DurationsContract.Columns.START_TIME
-            SortColumns.DURATION -> DurationsContract.Columns.DURATION
-        }
-        Log.d(TAG, "order is $order")
-
-        GlobalScope.launch {
-            val cursor = application.contentResolver.query(
-                DurationsContract.CONTENT_URI,
-                null,
-                selection,
-                selectionArgs,
-                order)
-            databaseCursor = cursor
-            reportAdapter.swapCursor(cursor)?.close()
-        }
-    }
 
 //    override fun onDestroy() {
 //        reportAdapter.swapCursor(null)?.close()
